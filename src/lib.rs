a004eb9683558a8aef3c7d0ee6168bbe74026bd6
Christian Mierau <christian.mierau@freenet.de>
2023-03-27 09:09:53 +0200

    hopefully fixed

--- src/lib.rs
+++ src/lib.rs
@@ -104,7 +104,7 @@ impl Threadpool {
                 self.used_threads -= 1;
             }
             self.thread_handles.remove(&th);
-            self.used_ids.remove(th - 1);
+            self.used_ids.retain(|x| th != *x);
             return Ok(());
         }
         Err(ThreadingError::ThreadNotFound)
